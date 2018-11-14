84ec08080aa952a467e1d93ebdc4a4791497bce2
Marcin Piekarski <kajnes@gmail.com>
2018-11-14 12:22:15 +0100

    DetailsClass - temp

---
+++ Windows Website Monitoring/DetailsClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Windows_Website_Monitoring
+{
+    public class DetailsClass
+    {
+        public string websiteUrl { get; set; }
+        public string websiteName { get; set; }
+        public string websiteStatus { get; set; }
+        public string websiteResponse { get; set; }
+    }
+}
